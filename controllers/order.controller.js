d602940b27e3aa7548cd6cf9b1feea203c873b25
val <valentin.lacour@viacesi.fr>
2023-07-05 15:12:22 +0200

    change api

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -162,10 +162,6 @@ exports.updateStatusOrder = async (req, res, next) => {
             return res.status(401).json({message:"Unauthorized."})
         }
 
-        if (order.restaurant.toString() !== req.auth.userId) {
-            return res.status(401).json({message:"Unauthorized."})
-        }
-
         order.status = status;
         await order.save();
         res.status(200).json({message:"Order status is updated."})
